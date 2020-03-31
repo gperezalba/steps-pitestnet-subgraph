1f79717a52782662803a53dd5cbce93c2d91d94a
gperezalba <gperezalba94@gmail.com>
2020-03-31 16:36:11 +0200

    update

---
+++ src/nameService.ts
@@ -0,0 +1,41 @@
+import { Address } from "@graphprotocol/graph-ts"
+import { CreateName, ChangeWallet, ChangeOwner } from "../generated/NameService/NameService"
+import { Name, Wallet } from "../generated/schema"
+
+import { loadWallet } from "./identityFactory"
+
+export function handleCreateName(event: CreateName): void {
+    setWallet(event.params.name, event.params.wallet.toHexString());
+    setName(event.params.wallet, event.params.name);
+    setOwner(event.params.name, event.params.owner);
+}
+
+function setWallet(id: string, wallet: string): void {
+    let name = Name.load(id);
+
+    if (name == null) {
+        name = new Name(id);
+    }
+
+    name.wallet = wallet;
+    name.save();
+}
+
+function setName(walletAddress: Address, name: string): void {
+    loadWallet(walletAddress);
+    let wallet = Wallet.load(walletAddress.toHexString());
+
+    wallet.name = name;
+    wallet.save();
+}
+
+function setOwner(id: string, newOwner: Address): void {
+    let name = Name.load(id);
+
+    if (name == null) {
+        name = new Name(id);
+    }
+
+    name.owner = newOwner;
+    name.save();
+}
