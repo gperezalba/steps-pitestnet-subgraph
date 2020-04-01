cc3eaed877b31c8dbb711260e9cb62370e73f198
gperezalba <gperezalba94@gmail.com>
2020-04-01 11:59:58 +0200

    update

--- src/tokenFactory.ts
+++ src/tokenFactory.ts
@@ -1,12 +1,12 @@
-import { Address } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal } from "@graphprotocol/graph-ts"
 import { TokenCreated } from "../generated/TokenFactory/TokenFactory"
 import { Token } from "../generated/schema"
 
 export function handleCreatedToken(event: TokenCreated): void {
-    let token = Token.load(event.param._address.toHexString());
+    let token = Token.load(event.params._address.toHexString());
 
     if (token == null) {
-        token = new Token(event.param._address.toHexString());
+        token = new Token(event.params._address.toHexString());
         token.tokenSymbol = "TK"
         token.tokenName = "nametk"
         token.tokenDecimals = 18;
