84b0e9996d0f83ef08c61ccf19d826a09348774c
gperezalba <gperezalba94@gmail.com>
2020-04-01 19:52:59 +0200

    update

--- src/token.ts
+++ src/token.ts
@@ -38,13 +38,13 @@ export function newTransaction(event: Transfer): void {
         );
     }
 
-    if (event.params.from == Address.fromI32(0)) {
+    /*if (event.params.from == Address.fromI32(0)) {
         handleTokenMint(event.address.toString(), event.params.value.toBigDecimal());
     }
 
     if (event.params.to == Address.fromI32(0)) {
         handleTokenBurn(event.address.toString(), event.params.value.toBigDecimal());
-    }
+    }*/
 
     //pushWalletTransaction(tx as Transaction, event.params.to.toString());
     //pushWalletTransaction(tx as Transaction, event.params.from.toString());
