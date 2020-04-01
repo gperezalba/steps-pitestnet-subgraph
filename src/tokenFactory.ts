3bedc3de8d3da1d0021b9c05042771e9a335133b
gperezalba <gperezalba94@gmail.com>
2020-04-01 12:52:54 +0200

    update

--- src/tokenFactory.ts
+++ src/tokenFactory.ts
@@ -2,7 +2,7 @@ import { Address, BigDecimal } from "@graphprotocol/graph-ts"
 import { TokenCreated } from "../generated/TokenFactory/TokenFactory"
 import { TokenFactory } from "../generated/schema"
 
-export function handleCreatedToken(event: TokenCreated): void {
+export function handleTokenCreated(event: TokenCreated): void {
     let token = TokenFactory.load(event.params._address.toHexString());
 
     if (token == null) {
