76eb87faea1101bb396db02c7bce2b40522f3590
gperezalba <gperezalba94@gmail.com>
2020-04-01 12:44:48 +0200

    update

--- src/tokenFactory.ts
+++ src/tokenFactory.ts
@@ -3,12 +3,12 @@ import { TokenCreated } from "../generated/TokenFactory/TokenFactory"
 import { TokenFactory } from "../generated/schema"
 
 export function handleCreatedToken(event: TokenCreated): void {
-    /*let token = TokenFactory.load(event.params._address.toHexString());
+    let token = TokenFactory.load(event.params._address.toHexString());
 
     if (token == null) {
         token = new TokenFactory(event.params._address.toHexString());
         token.tkName = "TK"
     }
 
-    token.save();*/
+    token.save();
 }
