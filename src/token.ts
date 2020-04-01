0c79a175d12026b0a9d1233bed5836b020f60ff1
gperezalba <gperezalba94@gmail.com>
2020-04-01 11:00:23 +0200

    update

--- src/token.ts
+++ src/token.ts
@@ -16,22 +16,20 @@ export function handleNewToken(event: NewToken): void {
 }
 
 function addToken(tokenAddress: Address, event: NewToken): void {
-    //let token = Token.load(tokenAddress.toHexString());
+    let token = new Token(tokenAddress.toHexString());
+    token.tokenSymbol = "TK"
 
-    //if (token == null) {
+    /*let token = Token.load(tokenAddress.toHexString());
+
+    if (token == null) {
         let token = new Token(tokenAddress.toHexString());
-        /*let contract = TokenContract.bind(tokenAddress);
+        let contract = TokenContract.bind(tokenAddress);
     
         token.tokenSymbol = contract.symbol();
         token.tokenName = contract.name();
         token.tokenDecimals = contract.decimals();
-        token.totalSupply = contract.totalSupply().toBigDecimal();*/
-
-        token.tokenSymbol = event.params.symbol;
-        token.tokenName = event.params.name;
-        //token.tokenDecimals = contract.decimals();
-        //token.totalSupply = contract.totalSupply().toBigDecimal();
+        token.totalSupply = contract.totalSupply().toBigDecimal();
     
         token.save();
-    //} 
+    } */
 }
